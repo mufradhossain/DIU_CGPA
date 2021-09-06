705b568750d9e27b8b3a351cc9e06539820d5978
mufradhossain <79082294+mufradhossain@users.noreply.github.com>
2021-09-06 19:46:39 +0600

    Update app.py

--- app.py
+++ app.py
@@ -26,57 +26,60 @@ studentID = st.text_input("Enter your DIU ID")
 
 
 if st.button('Get CGPA'):
-    studentinfo_url = "http://software.diu.edu.bd:8189/result/studentInfo?studentId="+str(studentID)
-    semesterinfo_url = "http://software.diu.edu.bd:8189/result/semesterList"
-    response = urlopen(studentinfo_url)
-    studentinfo_json = json.loads(response.read())
-    response = urlopen(semesterinfo_url)
-    semesterinfo_json = json.loads(response.read())
-    semesterlength = len(semesterinfo_json)
-    semesters=[]
-    for x in range(semesterlength):
-        semesters.append(semesterinfo_json[x]['semesterId'])
-        fullresult={}
-        for semester in semesters:
-            result_url="http://software.diu.edu.bd:8189/result?grecaptcha=&semesterId="+str(semester)+"&studentId="+str(studentID)
-            response = urlopen(result_url)
-            semesterresult_json = json.loads(response.read())
-            fullresult[semesterresult_json[0]['semesterId']]=semesterresult_json
-            if semesterresult_json[0]['semesterId']== studentinfo_json['semesterId']:
-                break
+    try:
+        studentinfo_url = "http://software.diu.edu.bd:8189/result/studentInfo?studentId="+str(studentID)
+        semesterinfo_url = "http://software.diu.edu.bd:8189/result/semesterList"
+        response = urlopen(studentinfo_url)
+        studentinfo_json = json.loads(response.read())
+        response = urlopen(semesterinfo_url)
+        semesterinfo_json = json.loads(response.read())
+        semesterlength = len(semesterinfo_json)
+        semesters=[]
+        for x in range(semesterlength):
+            semesters.append(semesterinfo_json[x]['semesterId'])
+            fullresult={}
+            for semester in semesters:
+                result_url="http://software.diu.edu.bd:8189/result?grecaptcha=&semesterId="+str(semester)+"&studentId="+str(studentID)
+                response = urlopen(result_url)
+                semesterresult_json = json.loads(response.read())
+                fullresult[semesterresult_json[0]['semesterId']]=semesterresult_json
+                if semesterresult_json[0]['semesterId']== studentinfo_json['semesterId']:
+                    break
 
 
-    fullresult_keys = list(fullresult.keys())
+        fullresult_keys = list(fullresult.keys())
 
 
-    total_credits=0
-    total_cgpa=0
+        total_credits=0
+        total_cgpa=0
 
 
-    st.text("Name: "+ studentinfo_json["studentName"])
-    st.text('ID: '+studentinfo_json["studentId"])
-    st.text("Program: "+studentinfo_json["programName"])
+        st.text("Name: "+ studentinfo_json["studentName"])
+        st.text('ID: '+studentinfo_json["studentId"])
+        st.text("Program: "+studentinfo_json["programName"])
 
 
 
 
-    for y in fullresult_keys:
+        for y in fullresult_keys:
 
-        semester_credits=0
-        semester_sgpa=0
-
-
-        for x in range(len(fullresult[y])):
+            semester_credits=0
+            semester_sgpa=0
 
 
-            total_credits = total_credits + fullresult[y][x]['totalCredit']
+            for x in range(len(fullresult[y])):
 
 
-            total_cgpa = total_cgpa + ((fullresult[y][x]['pointEquivalent'])*(fullresult[y][x]['totalCredit']))
-    CGPA= total_cgpa/total_credits
+                total_credits = total_credits + fullresult[y][x]['totalCredit']
 
 
-    st.text("Total Credits Completed: "+ str(int(total_credits)))
-    number = 3.1415926
-    st.success(f"CGPA: {CGPA:.2f}")
-    
+                total_cgpa = total_cgpa + ((fullresult[y][x]['pointEquivalent'])*(fullresult[y][x]['totalCredit']))
+        CGPA= total_cgpa/total_credits
+
+
+        st.text("Total Credits Completed: "+ str(int(total_credits)))
+        number = 3.1415926
+        st.success(f"CGPA: {CGPA:.2f}")
+        
+    except:
+        st.warning("An error occurred. Try again later.")
